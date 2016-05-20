6e2d9e61cac9d605088809040df7136d7f710d64
Adalberto <Adalberto@192.168.100.169>
2016-05-20 13:11:51 -0300

    continuação incluir pesquisa de filiais...

--- src/main/java/br/com/engebras/controller/MbPesquisaFilial.java
+++ src/main/java/br/com/engebras/controller/MbPesquisaFilial.java
@@ -8,6 +8,15 @@ import java.util.List;
 
 import br.com.engebras.model.entities.Filial;
 import br.com.engebras.repository.filter.FilialFilter;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Adalberto
@@ -20,6 +29,8 @@ import br.com.engebras.repository.filter.FilialFilter;
 public class MbPesquisaFilial implements Serializable {
     
     private static final long serialVersionUID = 1L;
+
+    private EntityManager manager;
     
     public FilialFilter filtro;
     private List<Filial> filiaisFiltradas;
@@ -30,5 +41,36 @@ public class MbPesquisaFilial implements Serializable {
     public MbPesquisaFilial(){
         filtro = new FilialFilter();
     }
+
+    
+    public Filial guardar(Filial filial){
+        return manager.merge(filial);
+    }
+    
+    public void remover(Filial filial){
+        try{
+            filial = porNr_codigo(filial.getNr_codigo());
+            manager.remove(filial);
+            manager.flush();
+        } catch (PersistenceException e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Filial não pode ser excluída.", ""));
+        }
+    }
+
+    public List<Filial> filtrados(FilialFilter filtro) {
+        Session session = manager.unwrap(Session.class);
+        Criteria criteria = session.createCriteria(Filial.class);
+        
+        if (StringUtils.isNotBlank(filtro.getDc_filial())){
+            criteria.add(Restrictions.like("dc_filial",filtro.getDc_filial(), MatchMode.ANYWHERE));
+        }
+        
+    }
+
+    
+    public Filial porNr_codigo(int nr_codigo){
+        return manager.find(Filial.class, nr_codigo);
+    }
+
     
 }
