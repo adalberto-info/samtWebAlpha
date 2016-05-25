1e931cb44c89f239c231606e3e0c30cf0446c1a4
Adalberto <Adalberto@192.168.100.169>
2016-05-25 08:43:00 -0300

    continuação incluir pesquisa de filiais...

--- src/main/java/br/com/engebras/controller/MbPesquisaFilial.java
+++ src/main/java/br/com/engebras/controller/MbPesquisaFilial.java
@@ -8,9 +8,12 @@ import java.util.List;
 
 import br.com.engebras.model.entities.Filial;
 import br.com.engebras.filter.FilialFilter;
+import br.com.engebras.util.FacesContextUtil;
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -33,11 +36,10 @@ public class MbPesquisaFilial implements Serializable {
 
     private EntityManager manager;
     
-    public FilialFilter filtro;
+    public FilialFilter filtro ;
     private List<Filial> filiaisFiltradas;
 
     private Filial filialSelecionada;
-    
     
     public MbPesquisaFilial(){
         filtro = new FilialFilter();
@@ -61,9 +63,11 @@ public class MbPesquisaFilial implements Serializable {
     public void pesquisar(){
         filiaisFiltradas = filtrados(filtro);
     }
+
     
     public List<Filial> filtrados(FilialFilter filtro) {
-        Session session = manager.unwrap(Session.class);
+//        Session session = manager.unwrap(Session.class);
+        Session session = FacesContextUtil.getRequestSession();
         Criteria criteria = session.createCriteria(Filial.class);
         
         if (StringUtils.isNotBlank(filtro.getDc_filial())){
@@ -92,5 +96,16 @@ public class MbPesquisaFilial implements Serializable {
     public List<Filial> porDc_filial(String dc_filial) {
         return this.manager.createQuery("from filial where upper(dc_filial) like :dc_filial", Filial.class).setParameter("dc_filial", dc_filial.toUpperCase() + "%").getResultList();
     }
+
+    public List<Filial> getFiliaisFiltradas() {
+        return filiaisFiltradas;
+    }
+
+    public void setFiliaisFiltradas(List<Filial> filiaisFiltradas) {
+        this.filiaisFiltradas = filiaisFiltradas;
+    }
+ 
+        
+        
     
 }
