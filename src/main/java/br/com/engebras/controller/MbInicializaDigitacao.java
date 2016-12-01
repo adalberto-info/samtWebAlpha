515190ef9b5d5ff244aef7ed3b73311187b14601
Adalberto <Adalberto@192.168.100.169>
2016-12-01 09:01:02 -0200

    Continuação da criação de tela de iniciar digitação...

--- src/main/java/br/com/engebras/controller/MbInicializaDigitacao.java
+++ src/main/java/br/com/engebras/controller/MbInicializaDigitacao.java
@@ -20,6 +20,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Query;
 import org.hibernate.SQLQuery;
+import java.sql.SQLException;
+import org.hibernate.Transaction;
 
 import br.com.engebras.util.FacesContextUtil;
 import br.com.engebras.util.HibernateUtil;
@@ -27,7 +29,6 @@ import br.com.engebras.model.dao.InterfaceDAO;
 import br.com.engebras.model.dao.HibernateDAO;
 import br.com.engebras.model.entities.AutoInfracao;
 import br.com.engebras.model.entities.AutoInfracaoImagem;
-import org.hibernate.Transaction;
 
 @ManagedBean(name = "mbInicializaDigitacao")
 @SessionScoped
@@ -39,35 +40,23 @@ public class MbInicializaDigitacao implements Serializable {
         
     }
     
-    public String iniciaTabelasDigitacao(){
+    public void iniciaTabelasDigitacao(){
         String vlc_sql = "";
-
+        SQLQuery query;
+        int vln_resultado = 0;
+        
         Session session = FacesContextUtil.getRequestSession();
-//        Transaction transaction = session.beginTransaction();
 
         //excluindo todos os registros da tabela autoInfracao... 
-        try{
-            vlc_sql = "delete from autoInfracao ";
-            SQLQuery query = session.createSQLQuery(vlc_sql);
-        
-//            transaction.commit(); 
-        }
-        catch(Throwable t){
-//            transaction.rollback();
-            throw t;
-        }
+        vlc_sql = "delete autoInfracao ";
+        query = session.createSQLQuery(vlc_sql);
+        vln_resultado = query.executeUpdate();
         
         //excluindo todos os registros da tabela autoInfracaoImagem... 
-//        transaction = session.beginTransaction();
-        try{
-            vlc_sql = "delete from autoInfracaoImagem ";
-            SQLQuery query = session.createSQLQuery(vlc_sql);
+        vlc_sql = "delete from autoInfracaoImagem ";
+        query = session.createSQLQuery(vlc_sql);
+        vln_resultado = query.executeUpdate();
             
-//            transaction.commit();
-        }catch(Throwable t){
-//            transaction.rollback();
-            throw t;
-        }
         
         //inserindo registros na tabela autoInfracao... 
         
@@ -78,16 +67,15 @@ public class MbInicializaDigitacao implements Serializable {
         vlc_sql += "nr_tamanhoVeiculo, dc_placaOCR, nr_codTipoFiscalizacao, dc_numeroLaudo, dt_validadeLaudo, ";
         vlc_sql += "dt_verificacaoLaudo, dc_tipoIsencao, lg_renainf, nr_imagem, nr_codPais, lg_uso, nr_codClassificacaoTamanho,";
         vlc_sql += "nr_tempoAferido, nr_tempoLimite, nr_status, dt_envio,dt_recebe) ";
-	vlc_sql += "values ('000001012488718260', '', 0, 0, 0,0,0,0,'', 0, 6942, '0001', 50, 61, 54, '20161120', '110346', '0001', 1, 0, 74550, 0,1111, 'EPZ8570', 1, '4444', '20171231', '20161124', '1', 0, 1, 1, 0, 1, 0,0,1,'','') ";
+	vlc_sql += "values ('000001012488718260', '', 0, 0, 0,0,0,0,'', 0, 6942, '0001', 50, 61, 54, '20161120', '110346', '0001', 1, 0, 74550, 0,1111, 'EPZ8570', 1, '4444', '20171231', '20161124', '1', 0, 1, 1, 0, 1, 0,0,1,'0000-00-00','0000-00-00') ";
         
-        SQLQuery query = session.createSQLQuery(vlc_sql);
-        query.executeUpdate();
+        query = session.createSQLQuery(vlc_sql);
+        vln_resultado = query.executeUpdate();
 
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro inserido com sucesso!!!", ""));
 
         //inserindo registros na tabela autoInfracaoImagem... 
         
-        return "";
     }
     
 }
