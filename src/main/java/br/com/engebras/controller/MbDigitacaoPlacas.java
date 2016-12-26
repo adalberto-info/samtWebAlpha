0285edce5199cb884da5ede693ccf39ee054ad51
Adalberto <Adalberto@192.168.100.169>
2016-12-26 08:48:56 -0200

    Continuação criação da tela de digitação de placas...

--- src/main/java/br/com/engebras/controller/MbDigitacaoPlacas.java
+++ src/main/java/br/com/engebras/controller/MbDigitacaoPlacas.java
@@ -36,7 +36,9 @@ public class MbDigitacaoPlacas implements Serializable{
 
     private static final long serialVersionUID = 1L; 
     
-private List<String> images;
+    private AutoInfracao autoInfracao = new  AutoInfracao(); 
+    
+    private List<String> images;
      
     @PostConstruct
     public void init() {
@@ -51,7 +53,7 @@ private List<String> images;
     }
 
     public MbDigitacaoPlacas(){
-    
+        proximaInfracao();
     }
     
     private InterfaceDAO<AutoInfracao> autoInfracaoDAO() {
@@ -59,4 +61,67 @@ private List<String> images;
         return autoInfracaoDAO;
     }
 
+    public String limpaInfracao(){
+        autoInfracao = new AutoInfracao(); 
+        return editAutoInfracao(); 
+    }
+    
+    public String editAutoInfracao(){
+        return "/restrict/digitacaoPlacas.xhtml";
+    }
+
+    public String inicioPagina(){
+        return "/Inicio.xhtml";
+    }
+    
+    public boolean proximaInfracao(){
+        String vlc_sql = ""; 
+        int vln_resultado = 0;
+        String dc_nr_multa = " ";
+        List consInfracao;
+        SQLQuery query;
+        
+        Session session = FacesContextUtil.getRequestSession();
+        
+        vlc_sql = "update autoInfracao set lg_uso = 1, nr_usuarioDigitacao = 9000 where nr_status = 1 and lg_uso = 0 limit 1 ";
+
+        query = session.createSQLQuery(vlc_sql);
+        vln_resultado = query.executeUpdate();
+        
+        vlc_sql = "select a.* from autoInfracao a where a.nr_status = 1 and a.lg_uso = 1 and a.nr_usuarioDigitacao = 9000 limit 1 ";
+        
+        query = session.createSQLQuery(vlc_sql);
+        query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+        consInfracao = query.list();
+
+        for (Object oInfracao : consInfracao) {
+            Map row = (Map) oInfracao;
+            
+            dc_nr_multa = row.get("dc_nr_multa").toString();
+        }        
+        
+        if (!dc_nr_multa.equals(" ")){
+            autoInfracao = porDc_nr_multa(dc_nr_multa);
+            return true;
+        }else{
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Não existe mais infrações para digitação...", ""));
+            return false;
+        }
+        
+    }
+    
+    public AutoInfracao porDc_nr_multa(String dc_nr_multa) {
+
+        return autoInfracaoDAO().getEntity(dc_nr_multa);
+    }
+
+    public AutoInfracao getAutoInfracao() {
+        return autoInfracao;
+    }
+
+    public void setAutoInfracao(AutoInfracao autoInfracao) {
+        this.autoInfracao = autoInfracao;
+    }
+
+    
 }
